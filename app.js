94d178e52bfbce93dba4521812dce75831a93b15
Agata Liberska <liberska@gmail.com>
2021-03-06 21:30:23 +0000

    create progess update functionality

---
+++ app.js
@@ -0,0 +1,36 @@
+const progress = {
+    max: 1000,
+    current: 100,
+
+    getMax() {
+        return this.max;
+    }
+
+    getCurrent() {
+        return this.current;
+    }
+    
+    add(size) {
+        this.current += size;
+    },
+
+    remove(size) {
+        this.current -= size;
+    },
+
+    getRemaining() {
+        return this.max - this.current;
+    },
+
+    getProportionUsed() {
+        return this.current/this.max;
+    }
+}
+
+const progressBar = document.querySelector('.storage-progress');
+
+const updateProgressBar = () => {
+    progressBar.style.width = `${progress.getProportionUsed()*100}%`;
+}
+
+updateProgressBar();
