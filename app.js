81fe9d56a4341d8bb89f517040c78c286a7ea251
Agata Liberska <liberska@gmail.com>
2021-03-07 01:06:10 +0000

    display amount of storage used

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ const progress = {
 }
 
 const progressBar = document.querySelector('.component__storage__progress');
-
+const used = document.querySelector('.component__storage__used');
 const modal = document.querySelector('.component__storage__modal__num');
 
 const updateProgressBar = () => {
     progressBar.style.width = `${progress.getProportionUsed()*100}%`;
-
+    used.textContent = `${progress.getCurrent()} GB`;
     modal.textContent = `${progress.getRemaining()}`;
 }
 
