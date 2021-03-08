f02022ce364d68e1a53aef8e0598efcdb3f4f12a
Agata Liberska <liberska@gmail.com>
2021-03-08 13:54:08 +0000

    add changemax method to progress

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ class Progress {
     
     add(size) {
         this.current += size;
+        this.onChange();
     }
 
     remove(size) {
         this.current -= size;
+        this.onChange();
     }
 
     getRemaining() {
@@ -28,20 +30,27 @@ class Progress {
     getProportionUsed() {
         return this.current/this.max;
     }
-}
 
-const storage = new Progress(1000,815);
+    changeMax(max) {
+        this.max = max;
+        this.onChange();
+    }
+
+    onChange() {
+
+    }
+}
 
 const progressBar = document.querySelector('.component__storage__progress');
 const used = document.querySelector('.component__storage__used');
 const modal = document.querySelector('.component__storage__modal__num');
-
-
+const max = document.querySelector('.component__storage__max');
 
 const updateProgressBar = () => {
     progressBar.style.width = `${storage.getProportionUsed()*100}%`;
     used.textContent = `${storage.getCurrent()} GB`;
     modal.textContent = `${storage.getRemaining()}`;
+    max.textContent = `${storage.getMax()}`;
     if (storage.getCurrent() >= storage.getMax()) {
         used.style.color = `hsl(335, 100%, 65%)`;
         modal.style.color = `hsl(335, 100%, 65%)`;
@@ -50,6 +59,15 @@ const updateProgressBar = () => {
         modal.style.color = `#0F1B3D`; 
     }
 }
+
+const storage = new Progress(1000,815);
+storage.onChange = updateProgressBar
+
+
+
+
+
+
 
 updateProgressBar();
 
